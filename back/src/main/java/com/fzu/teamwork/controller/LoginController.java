ba88a04ff040187ee9409636de6b463ca466fd67
liuchenghua <547173318@qq.com>
2020-04-14 23:20:51 +0800

    11
    11

--- back/src/main/java/com/fzu/teamwork/controller/LoginController.java
+++ back/src/main/java/com/fzu/teamwork/controller/LoginController.java
@@ -1,10 +1,23 @@
 package com.fzu.teamwork.controller;
 
 
+import com.fzu.teamwork.dao.UserDao;
+import com.fzu.teamwork.model.AjaxResponse;
+import com.fzu.teamwork.model.User;
+import com.fzu.teamwork.view.UserVO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @Slf4j
 @RestController
 public class LoginController {
+    @GetMapping("/user")
+    public @ResponseBody
+    AjaxResponse getUser(@RequestBody User user){
+        UserVO userVO=new UserVO(user);
+        return AjaxResponse.success(userVO);
+    }
+
 }
--- back/src/main/java/com/fzu/teamwork/controller/UserController.java
+++ back/src/main/java/com/fzu/teamwork/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.fzu.teamwork.controller;
 
 
+import com.fzu.teamwork.model.AjaxResponse;
+import com.fzu.teamwork.model.User;
+import com.fzu.teamwork.view.UserVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class UserController {
+
+    
 }
