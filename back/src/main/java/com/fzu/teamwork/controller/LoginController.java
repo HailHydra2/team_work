188dc32833987285c696f02d4908c5511673d44f
fzuwsh <1306321810@qq.com>
2020-04-15 11:04:03 +0800

    MessageController

--- back/src/main/java/com/fzu/teamwork/controller/LoginController.java
+++ back/src/main/java/com/fzu/teamwork/controller/LoginController.java
@@ -1,10 +1,21 @@
 package com.fzu.teamwork.controller;
 
 
+import com.fzu.teamwork.model.AjaxResponse;
+import com.fzu.teamwork.view.UserVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class LoginController {
+
+    @GetMapping("/user")
+    public @ResponseBody
+    AjaxResponse xx(){
+        UserVO u = new UserVO();
+        return AjaxResponse.success(u);
+    }
 }
--- back/src/main/java/com/fzu/teamwork/controller/MessageController.java
+++ back/src/main/java/com/fzu/teamwork/controller/MessageController.java
@@ -1,10 +1,19 @@
 package com.fzu.teamwork.controller;
 
 
+import com.fzu.teamwork.model.AjaxResponse;
+import com.fzu.teamwork.view.MessagePage;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class MessageController {
+
+    @GetMapping("/userMessages/{uid}")
+    public @ResponseBody
+    AjaxResponse getMessagePage(@PathVariable int uid, @RequestBody MessagePage page){
+
+        return AjaxResponse.success();
+    }
 }
