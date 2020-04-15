571cd9883884696f2ef5778dcd09dff53bbac953
ZhangJiaWeiDEV <49120645+ZhangJiaWeiDEV@users.noreply.github.com>
2020-04-15 14:02:40 +0800

    更新问题控制器和举报控制器
    新增QuestionController中获取问题列表/新增问题/获取我的问题/获取关注问题四个静态功能
    新增ReportController中获取被举报问题/获取被举报回复两个静态功能

--- back/src/main/java/com/fzu/teamwork/controller/QuestionController.java
+++ back/src/main/java/com/fzu/teamwork/controller/QuestionController.java
@@ -1,10 +1,116 @@
 package com.fzu.teamwork.controller;
 
 
+import com.fzu.teamwork.dao.QuestionDao;
+import com.fzu.teamwork.model.*;
+import com.fzu.teamwork.view.MessagePage;
+import com.fzu.teamwork.view.QuestionPage;
+import com.fzu.teamwork.view.QuestionVO;
+import com.fzu.teamwork.view.UserVO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestController
 public class QuestionController {
+    @Resource
+    private QuestionDao questionDao;
+
+    @GetMapping("/questions")
+    public @ResponseBody AjaxResponse getQuestionPage(@RequestBody QuestionPage questionPage){
+        questionPage.setPageNum(10);
+        List<Integer> buttonList = new ArrayList<>();
+        buttonList.add(1);
+        buttonList.add(2);
+        questionPage.setButtonList(buttonList);
+        questionPage.setHasPrevious(false);
+        questionPage.setHasNext(true);
+        List<QuestionVO> questionList = new ArrayList<>();
+        QuestionVO question1 = new QuestionVO();
+        question1.setTitle("hello");
+        question1.setContent("hello world");
+        questionList.add(question1);
+        QuestionVO question2 = new QuestionVO();
+        question2.setTitle("goodbye");
+        question2.setContent("goodbye world");
+        questionList.add(question2);
+        questionPage.setQuestions(questionList);
+        return AjaxResponse.success(questionPage);
+    }
+
+    @PostMapping("/question")
+    public  @ResponseBody AjaxResponse addQuestion(@RequestBody QuestionVO questionVO){
+        Title title = new Title();
+        title.setTitle(questionVO.getTitle());
+        title.setId(questionVO.getQuestion().getContentId());
+
+        Content content = new Content();
+        content.setContent(questionVO.getContent());
+        content.setId(questionVO.getQuestion().getContentId());
+
+        AccountData accountData = new AccountData();
+        accountData.setId(1);
+        accountData.setUserId(1);
+        accountData.setLevel(10);
+        accountData.setScore(10);
+        accountData.setExperienceValue(99/100);
+        accountData.setFocusNum(5);
+        accountData.setQuestionNum(5);
+        accountData.setResponseNum(5);
+
+        User user = new User();
+        user.setId(1);
+        user.setAccount("221701421");
+        user.setPassword("123456");
+        user.setName("wsh");
+        user.setIdCard("123456789");
+        user.setIdentity("student");
+        user.setPhoneNum("110");
+        user.setAccountDataId(1);
+
+        UserVO userVO = new UserVO();
+        userVO.setUser(user);
+        userVO.setAccountData(accountData);
+        return AjaxResponse.success(userVO);
+    }
+
+    @GetMapping("/userQuestions/{uid}")
+    public @ResponseBody AjaxResponse getUserQuestionPage(@PathVariable String uid,  @RequestBody QuestionPage questionPage){
+        questionPage.setPageNum(10);
+        List<Integer> buttonList = new ArrayList<>();
+        buttonList.add(1);
+        buttonList.add(2);
+        questionPage.setButtonList(buttonList);
+        questionPage.setHasPrevious(false);
+        questionPage.setHasNext(true);
+        List<QuestionVO> questionList = new ArrayList<>();
+        QuestionVO question1 = new QuestionVO();
+        question1.setTitle("hello");
+        question1.setContent("hello world");
+        questionList.add(question1);
+        questionPage.setQuestions(questionList);
+        return AjaxResponse.success(questionPage);
+    }
+
+    @GetMapping("/userAttentions/{uid}")
+    public @ResponseBody AjaxResponse getAttentionQuestionPage(@PathVariable String uid, @RequestBody QuestionPage questionPage){
+        questionPage.setPageNum(10);
+        List<Integer> buttonList = new ArrayList<>();
+        buttonList.add(1);
+        buttonList.add(2);
+        questionPage.setButtonList(buttonList);
+        questionPage.setHasPrevious(false);
+        questionPage.setHasNext(true);
+        List<QuestionVO> questionList = new ArrayList<>();
+        QuestionVO question1 = new QuestionVO();
+        question1.setTitle("goodbye");
+        question1.setContent("goodbye world");
+        questionList.add(question1);
+        questionPage.setQuestions(questionList);
+        return AjaxResponse.success(questionPage);
+    }
 }
--- back/src/main/java/com/fzu/teamwork/controller/ReportController.java
+++ back/src/main/java/com/fzu/teamwork/controller/ReportController.java
@@ -1,10 +1,47 @@
 package com.fzu.teamwork.controller;
 
 
+import com.fzu.teamwork.model.Question;
+import com.fzu.teamwork.model.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestController
 public class ReportController {
+    @GetMapping("/questionReports")
+    public List<Question> getReportQuestion(){
+        List<Question> questionList= new ArrayList<>();
+        Question question1 = new Question();
+        question1.setId(1);
+        question1.setAutherId(1);
+        question1.setReportNum(1);
+        questionList.add(question1);
+        Question question2 = new Question();
+        question1.setId(2);
+        question1.setAutherId(1);
+        question1.setReportNum(1);
+        questionList.add(question2);
+        return questionList;
+    }
+
+    @GetMapping("/responseReports")
+    public List<Response> getReportResponse(){
+        List<Response> responseList = new ArrayList<>();
+        Response response1 = new Response();
+        response1.setId(1);
+        response1.setAuthorId(1);
+        response1.setReportNum(1);
+        responseList.add(response1);
+        Response response2 = new Response();
+        response2.setId(2);
+        response2.setAuthorId(1);
+        response2.setReportNum(1);
+        responseList.add(response2);
+        return responseList;
+    }
 }
