f13357f8725e11e3e1d36ba813ba34b7fac42c2e
fzuwsh <1306321810@qq.com>
2020-04-27 18:45:52 +0800

    数据库更新

--- sql/team_work.sql
+++ sql/team_work.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50562
 File Encoding         : 65001
 
-Date: 2020-04-19 15:30:57
+Date: 2020-04-27 18:45:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,11 +28,13 @@ CREATE TABLE `account_data` (
   `question_num` int(11) NOT NULL,
   `response_num` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of account_data
 -- ----------------------------
+INSERT INTO `account_data` VALUES ('-1', '0', '0', '0', '0', '0', '0');
+INSERT INTO `account_data` VALUES ('1', '12', '231', '40', '10', '3', '36');
 
 -- ----------------------------
 -- Table structure for `attention`
@@ -54,8 +56,6 @@ CREATE TABLE `attention` (
 -- ----------------------------
 -- Records of attention
 -- ----------------------------
-INSERT INTO `attention` VALUES ('1', '1', '1', '2020-10-21 20:19:08', '1');
-INSERT INTO `attention` VALUES ('2', '1', '2', '2020-10-21 20:19:08', '1');
 
 -- ----------------------------
 -- Table structure for `block`
@@ -80,14 +80,13 @@ CREATE TABLE `content` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` varchar(1000) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of content
 -- ----------------------------
-INSERT INTO `content` VALUES ('1', 'content1');
-INSERT INTO `content` VALUES ('2', 'content2');
-INSERT INTO `content` VALUES ('3', 'content3');
+INSERT INTO `content` VALUES ('18', 'content1');
+INSERT INTO `content` VALUES ('19', 'content2');
 
 -- ----------------------------
 -- Table structure for `like`
@@ -118,7 +117,7 @@ CREATE TABLE `message` (
   PRIMARY KEY (`id`),
   KEY `message_object_id` (`object_id`),
   CONSTRAINT `message_object_id` FOREIGN KEY (`object_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of message
@@ -138,15 +137,15 @@ CREATE TABLE `question` (
   PRIMARY KEY (`id`),
   KEY `question_author_id` (`author_id`),
   KEY `question_content_id` (`content_id`),
-  CONSTRAINT `question_content_id` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`),
-  CONSTRAINT `question_author_id` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  CONSTRAINT `question_author_id` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `question_content_id` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of question
 -- ----------------------------
-INSERT INTO `question` VALUES ('1', '1', '5', '0', '2020-04-07 21:31:21', '2');
-INSERT INTO `question` VALUES ('2', '1', '1', '0', '2020-04-14 21:31:41', '1');
+INSERT INTO `question` VALUES ('15', '1', '0', '0', '2020-04-27 17:11:15', '18');
+INSERT INTO `question` VALUES ('16', '1', '0', '0', '2020-04-27 17:14:12', '19');
 
 -- ----------------------------
 -- Table structure for `question_title`
@@ -159,13 +158,15 @@ CREATE TABLE `question_title` (
   PRIMARY KEY (`id`),
   KEY `title_question_id` (`question_id`),
   KEY `question_title_id` (`title_id`),
-  CONSTRAINT `title_question_id` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`),
-  CONSTRAINT `question_title_id` FOREIGN KEY (`title_id`) REFERENCES `title` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `question_title_id` FOREIGN KEY (`title_id`) REFERENCES `title` (`id`),
+  CONSTRAINT `title_question_id` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of question_title
 -- ----------------------------
+INSERT INTO `question_title` VALUES ('10', '15', '10');
+INSERT INTO `question_title` VALUES ('11', '16', '11');
 
 -- ----------------------------
 -- Table structure for `report_question`
@@ -227,13 +228,11 @@ CREATE TABLE `response` (
   CONSTRAINT `response_author_id` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`),
   CONSTRAINT `response_content_id` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`),
   CONSTRAINT `response_question_id` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of response
 -- ----------------------------
-INSERT INTO `response` VALUES ('1', '1', '1', '1', '3', '2', '72', '2020-04-22 21:32:38');
-INSERT INTO `response` VALUES ('2', '2', '1', '2', '1', '32', '18', '2020-04-01 21:33:11');
 
 -- ----------------------------
 -- Table structure for `reward`
@@ -248,15 +247,26 @@ CREATE TABLE `reward` (
   PRIMARY KEY (`id`),
   KEY `reward_user_id` (`user_id`),
   CONSTRAINT `reward_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of reward
 -- ----------------------------
-INSERT INTO `reward` VALUES ('1', '1', 'SyntheticTest', '10', '2020-04-22 21:06:37');
-INSERT INTO `reward` VALUES ('2', '1', 'ServiceTime', '21', '2020-04-15 21:07:39');
-INSERT INTO `reward` VALUES ('3', '1', 'SyntheticTest', '2', '2020-03-12 01:12:42');
-INSERT INTO `reward` VALUES ('4', '1', 'SyntheticTest', '13', '2020-05-31 21:13:13');
+INSERT INTO `reward` VALUES ('18', '1', 'ServiceTime', '1', '2020-04-25 19:47:37');
+INSERT INTO `reward` VALUES ('19', '1', 'ServiceTime', '1', '2020-04-25 20:04:26');
+INSERT INTO `reward` VALUES ('20', '1', 'ServiceTime', '1', '2020-04-25 20:04:35');
+INSERT INTO `reward` VALUES ('21', '1', 'ServiceTime', '1', '2020-04-25 20:04:46');
+INSERT INTO `reward` VALUES ('22', '1', 'SyntheticTest', '0.1', '2020-04-25 20:04:51');
+INSERT INTO `reward` VALUES ('23', '1', 'SyntheticTest', '0.1', '2020-04-25 20:06:08');
+INSERT INTO `reward` VALUES ('24', '1', 'SyntheticTest', '0.1', '2020-04-25 20:06:36');
+INSERT INTO `reward` VALUES ('25', '1', 'SyntheticTest', '0.1', '2020-04-25 20:06:44');
+INSERT INTO `reward` VALUES ('26', '1', 'SyntheticTest', '0.1', '2020-04-25 20:34:37');
+INSERT INTO `reward` VALUES ('27', '1', 'SyntheticTest', '0.1', '2020-04-25 20:34:38');
+INSERT INTO `reward` VALUES ('28', '1', 'SyntheticTest', '0.1', '2020-04-25 20:34:39');
+INSERT INTO `reward` VALUES ('29', '1', 'SyntheticTest', '0.1', '2020-04-25 20:34:39');
+INSERT INTO `reward` VALUES ('30', '1', 'SyntheticTest', '0.1', '2020-04-25 20:47:52');
+INSERT INTO `reward` VALUES ('31', '1', 'ServiceTime', '1', '2020-04-25 20:47:55');
+INSERT INTO `reward` VALUES ('32', '1', 'SyntheticTest', '0.1', '2020-04-27 15:00:42');
 
 -- ----------------------------
 -- Table structure for `title`
@@ -266,11 +276,13 @@ CREATE TABLE `title` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of title
 -- ----------------------------
+INSERT INTO `title` VALUES ('10', 'test1');
+INSERT INTO `title` VALUES ('11', 'test2');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -282,15 +294,17 @@ CREATE TABLE `user` (
   `password` varchar(50) NOT NULL,
   `name` varchar(50) NOT NULL,
   `id_card` varchar(20) NOT NULL,
-  `identity` varchar(10) NOT NULL DEFAULT '学生',
+  `identity` varchar(10) NOT NULL DEFAULT 'student',
   `phone_num` varchar(12) DEFAULT NULL,
   `account_data_id` int(11) DEFAULT '-1',
   PRIMARY KEY (`id`),
   KEY `user_account_data_id` (`account_data_id`),
   CONSTRAINT `user_account_data_id` FOREIGN KEY (`account_data_id`) REFERENCES `account_data` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '221701421', '1234', 'wsh', '123', 'student', null, null);
+INSERT INTO `user` VALUES ('1', '123', '1', 'wsh', '123', 'student', null, '1');
+INSERT INTO `user` VALUES ('2', '221701422', '1234', 'wsh2', '124', 'studnet', null, '-1');
+INSERT INTO `user` VALUES ('3', '221701424', '1235', 'wsh3', '125', 'studnet', null, '-1');
