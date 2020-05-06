06e760b5372e82a4828285d724fde45f84148904
fzuwsh <1306321810@qq.com>
2020-05-06 19:55:50 +0800

    复制一份给测试云端代码

---
+++ front_web/adminInterface/assets/js/biz/reward.js
@@ -0,0 +1,369 @@
+
+
+jQuery(function ($) {
+  var grid_selector = "#grid-table";
+  var pager_selector = "#grid-pager";
+
+  jQuery(grid_selector).jqGrid({
+    url: "http://118.190.90.167:8888/rewards",
+    loadonce: true,
+    loadBeforeSend: function(jqXHR) {
+      jqXHR.setRequestHeader("token", userVO.token);
+    },
+    mtype: "get",
+    //data: grid_data,
+    datatype: "json",
+    //    mtype:"POST",
+    //datatype:"local",
+    height: 250,
+    colNames: ['编号', '用户学号', '姓名', '申请时间', '申请类别', '申请数量'],
+    colModel: [
+      {
+        name: 'reward.id',
+        index: 'reward.id',
+        width: 100,
+        //  sorttype: "int",
+        editable: true
+      },
+      {
+        name: 'account',
+        index: 'account',
+        //  sorttype: "int",
+        width: 100,
+        editable: true,
+      },
+      {
+        name: 'name',
+        index: 'name',
+        width: 100,
+        //  sorttype: "int",
+        editable: true
+      },
+      {
+        name: 'reward.applyTime',
+        index: 'reward.applyTime',
+        width: 200,
+      },
+      {
+        name: 'reward.type',
+        index: 'reward.type',
+        width: 100,
+        //  sorttype: "int",
+        editable: true
+      },
+      {
+        name: 'reward.rewardNum',
+        index: 'reward.rewardNum',
+        width: 100,
+        sorttype: "int",
+        editable: true
+      }
+    ],
+
+    width: 80,
+    fixed: true,
+    sortname: 'id',
+    viewrecords: true,
+    rowNum: 10,
+    rowList: [10, 20, 30],
+    pager: pager_selector,
+    altRows: true,
+    multiselect: true,
+
+
+    loadComplete: function () {
+      var table = this;
+
+      setTimeout(function () {
+        updateActionIcons(table);
+        updatePagerIcons(table);
+        enableTooltips(table);
+      }, 0);
+    },
+    //editurl: 'server.php', //nothing is saved
+    editurl: $path_base, //nothing is saved
+    caption: "积分信息操作",
+    autowidth: true
+  });
+
+  //enable search/filter toolbar
+  //jQuery(grid_selector).jqGrid('filterToolbar',{defaultSearch:true,stringResult:true})
+
+  //switch element when editing inline
+  function aceSwitch(cellvalue, options, cell) {
+    setTimeout(function () {
+      $(cell).find('input[type=checkbox]')
+        .wrap('<label class="inline" />')
+        .addClass('ace ace-switch ace-switch-5')
+        .after('<span class="lbl"></span>');
+    }, 0);
+  }
+
+  //enable datepicker
+  /*function pickDate( cellvalue, options, cell ) {
+    setTimeout(function(){
+      $(cell) .find('input[type=text]')
+          .datepicker({format:'yyyy-mm-dd' , autoclose:true});
+    }, 0);
+  }
+
+*/
+  //navButtons
+  jQuery(grid_selector).jqGrid('navGrid', pager_selector, { //navbar options
+    edit: false,
+    editicon: 'icon-pencil blue',
+    add: false,
+    addicon: 'icon-plus-sign purple',
+    del: false,
+    delicon: 'icon-trash red',
+    search: true,
+    searchicon: 'icon-search orange',
+    refresh: true,
+    refreshicon: 'icon-refresh green',
+    view: true,
+    viewicon: 'icon-zoom-in grey',
+  }, {
+
+    //查询的表格设置
+    //edit record form
+    closeAfterEdit: true,
+    recreateForm: true,
+    beforeShowForm: function (e) {
+      var form = $(e[0]);
+      form.closest('.ui-jqdialog').find('.ui-jqdialog-titlebar').wrapInner('<div class="widget-header" />')
+      style_edit_form(form);
+    }
+  },
+    {
+      //增加行内容设置
+      //new record form
+      closeAfterAdd: true,
+      recreateForm: true,
+      viewPagerButtons: false,
+      //设置新增表格样式
+      beforeShowForm: function (e) {
+        var form = $(e[0]);
+        form.closest('.ui-jqdialog').find('.ui-jqdialog-titlebar').wrapInner('<div class="widget-header" />')
+        style_edit_form(form);
+      },
+      url: 'service-add.php',
+      // onSubmit: function (params, postdata) {
+      //   var submitButton = document.getElementById("sData");
+      //   submitButton.onclick = function () {
+      //     alert("你点击了按钮哦！");
+      //   }
+      //   var value = {
+      //     "id": (document.getElementById("id")),
+      //   };
+      //   Service.appendData(value).then(function (data) {
+      //     console.log(data);
+      //     $(this).jqGrid().trigger('reloadGrid');
+      //   });
+      //   return true;
+      // },
+      afterSubmit: function (response, postdata) {
+        if (response.responseText != "") {
+          $(this).jqGrid('setGridParam', {
+            datatype: 'json'
+          }).trigger('reloadGrid');
+          alert(response.responseText);
+          return [true, response.responseText]
+        }
+      }
+    },
+    {
+      //delete record form
+      delData: {
+        delId: function () {
+          var sel_id = [];
+          sel_id = $(grid_selector).jqGrid('getGridParam',
+            'selarrrow');
+          var value = '';
+          for (var i = 0; i < sel_id.length; i++) {
+            value = value + ',' + $(grid_selector).jqGrid('getCell',
+              sel_id[i], 'id');
+          }
+          if (value.charAt(0) == ',') {
+            value = value.substr(1);
+          }
+          Service.delete(value).then(function (data) {
+            console.log(data);
+            $(this).jqGrid().trigger('reloadGrid');
+          });
+          return value;
+        }
+      },
+      recreateForm: true,
+      beforeShowForm: function (e) {
+        var form = $(e[0]);
+        if (form.data('styled')) return false;
+        form.closest('.ui-jqdialog').find('.ui-jqdialog-titlebar').wrapInner('<div class="widget-header" />')
+        style_delete_form(form);
+        form.data('styled', true);
+      },
+      /*onClick : function(e) {
+        alert("1");
+      }*/
+      closeOnEscape: true,
+      closeAfterDelete: true,
+      reloadAfterSubmit: true,
+      drag: true,
+    },
+    {
+      //search form
+      recreateForm: true,
+      afterShowSearch: function (e) {
+        var form = $(e[0]);
+        form.closest('.ui-jqdialog').find('.ui-jqdialog-title').wrap('<div class="widget-header" />')
+        style_search_form(form);
+      },
+      afterRedraw: function () {
+        style_search_filters($(this));
+      },
+      multipleSearch: true,
+
+      //  multipleGroup:true,
+      // showQuery: true
+
+    }, {
+    //view record form
+    recreateForm: true,
+    beforeShowForm: function (e) {
+      var form = $(e[0]);
+      form.closest('.ui-jqdialog').find('.ui-jqdialog-title').wrap('<div class="widget-header" />')
+    }
+  })
+
+  function style_edit_form(form) {
+    //enable datepicker on "sdate" field and switches for "stock" field
+    /*   form.find('input[name=sdate]').datepicker({format:'yyyy-mm-dd' , autoclose:true})
+         .end().find('input[name=stock]')
+             .addClass('ace ace-switch ace-switch-5').wrap('<label class="inline" />').after('<span class="lbl"></span>');
+   */
+    //update buttons classes
+    var buttons = form.next().find('.EditButton .fm-button');
+    buttons.addClass('btn btn-sm').find('[class*="-icon"]').remove();//ui-icon, s-icon
+    buttons.eq(0).addClass('btn-primary').prepend('<i class="icon-ok"></i>');
+    buttons.eq(1).prepend('<i class="icon-remove"></i>')
+
+    buttons = form.next().find('.navButton a');
+    buttons.find('.ui-icon').remove();
+    buttons.eq(0).append('<i class="icon-chevron-left"></i>');
+    buttons.eq(1).append('<i class="icon-chevron-right"></i>');
+  }
+
+  //设置点击删除的按钮
+  function style_delete_form(form) {
+    var buttons = form.next().find('.EditButton .fm-button');
+    buttons.addClass('btn btn-sm').find('[class*="-icon"]').remove(); //ui-icon, s-icon
+    buttons.eq(0).addClass('btn-danger').prepend('<i class="icon-trash"></i>');
+    buttons.eq(1).prepend('<i class="icon-remove"></i>')
+  }
+
+  //查询表格
+  function style_search_filters(form) {
+    form.find('.delete-rule').val('X');
+    form.find('.add-rule').addClass('btn btn-xs btn-primary');
+    form.find('.add-group').addClass('btn btn-xs btn-success');
+    form.find('.delete-group').addClass('btn btn-xs btn-danger');
+  }
+
+  //设置查询的样式
+  function style_search_form(form) {
+    var dialog = form.closest('.ui-jqdialog');
+    var buttons = dialog.find('.EditTable')
+    buttons.find('.EditButton a[id*="_reset"]').addClass('btn btn-sm btn-info').find('.ui-icon').attr('class', 'icon-retweet');
+    buttons.find('.EditButton a[id*="_query"]').addClass('btn btn-sm btn-inverse').find('.ui-icon').attr('class', 'icon-comment-alt');
+    buttons.find('.EditButton a[id*="_search"]').addClass('btn btn-sm btn-purple').find('.ui-icon').attr('class', 'icon-search');
+  }
+
+  function beforeDeleteCallback(e) {
+    var form = $(e[0]);
+    if (form.data('styled')) return false;
+    form.closest('.ui-jqdialog').find('.ui-jqdialog-titlebar').wrapInner('<div class="widget-header" />')
+    style_delete_form(form);
+    form.data('styled', true);
+  }
+
+  function beforeEditCallback(e) {
+    var form = $(e[0]);
+    form.closest('.ui-jqdialog').find('.ui-jqdialog-titlebar').wrapInner('<div class="widget-header" />')
+    style_edit_form(form);
+  }
+
+
+  //it causes some flicker when reloading or navigating grid
+  //it may be possible to have some custom formatter to do this as the grid is being created to prevent this
+  //or go back to default browser checkbox styles for the grid
+  function styleCheckbox(table) {
+    /**
+      $(table).find('input:checkbox').addClass('ace')
+      .wrap('<label />')
+      .after('<span class="lbl align-top" />')
+
+
+      $('.ui-jqgrid-labels th[id*="_cb"]:first-child')
+      .find('input.cbox[type=checkbox]').addClass('ace')
+      .wrap('<label />').after('<span class="lbl align-top" />');
+    */
+  }
+
+
+  //unlike navButtons icons, action icons in rows seem to be hard-coded
+  //you can change them like this in here if you want
+  function updateActionIcons(table) {
+
+    /*	var replacement =
+      {
+        'ui-icon-pencil' : 'icon-pencil blue',
+        'ui-icon-trash' : 'icon-trash red',
+        'ui-icon-disk' : 'icon-ok green',
+        'ui-icon-cancel' : 'icon-remove red'
+      };
+      $(table).find('.ui-pg-div span.ui-icon').each(function(){
+        var icon = $(this);
+        var $class = $.trim(icon.attr('class').replace('ui-icon', ''));
+        if($class in replacement) icon.attr('class', 'ui-icon '+replacement[$class]);
+      })*/
+
+  }
+
+  //换页设置
+  //replace icons with FontAwesome icons like above
+  function updatePagerIcons(table) {
+    var replacement = {
+      'ui-icon-seek-first': 'icon-double-angle-left bigger-140',
+      'ui-icon-seek-prev': 'icon-angle-left bigger-140',
+      'ui-icon-seek-next': 'icon-angle-right bigger-140',
+      'ui-icon-seek-end': 'icon-double-angle-right bigger-140'
+    };
+    $('.ui-pg-table:not(.navtable) > tbody > tr > .ui-pg-button > .ui-icon').each(function () {
+      var icon = $(this);
+      var $class = $.trim(icon.attr('class').replace('ui-icon', ''));
+
+      if ($class in replacement) icon.attr('class', 'ui-icon ' + replacement[$class]);
+    })
+  }
+
+  function enableTooltips(table) {
+    $('.navtable .ui-pg-button').tooltip({ container: 'body' });
+    $(table).find('.ui-pg-div').tooltip({ container: 'body' });
+  }
+
+  Date.prototype.Format = function (fmt) {
+    var o = {
+      "M+": this.getMonth() + 1, //月份 
+      "d+": this.getDate(), //日 
+      "h+": this.getHours(), //小时 
+      "m+": this.getMinutes(), //分 
+      "s+": this.getSeconds(), //秒 
+      "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+      "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+      if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+  }
+  //var selr = jQuery(grid_selector).jqGrid('getGridParam','selrow');
+});
---
+++ front_web/js/util.js
@@ -0,0 +1,377 @@
+
+//用于获取url种name参数的值，不存在返回false
+function getQueryVariable(name)
+{
+    name = name.replace(/[]/,"\[").replace(/[]/,"\[").replace(/[]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec(window.parent.location.href);
+    if( results == null ) 
+        return ""; 
+    else {
+        return decodeURI(results[1]);
+    }
+}
+
+
+
+//问题页面换页
+function changeQuestionPage(page, path){
+    $.ajax({
+        url: path,
+        type: "post", 
+        data:JSON.stringify(page),
+        beforeSend: function (request) {
+            request.setRequestHeader("token", userVO.token);
+        },
+        contentType: 'application/json;charset=utf-8',
+        success: function (data) {
+            page = data.data;
+            console.info(page);
+            updateList(page);
+        }
+    });
+}
+
+// //回复页面换页
+// function changeQuestionPage(page, path){
+//     $.ajax({
+//         url: path,
+//         type: "post", 
+//         data:JSON.stringify(page),
+//         contentType: 'application/json;charset=utf-8',
+//         success: function (data) {
+//             page = data.data;
+//             console.info(page);
+//             updateList(page);
+//         }
+//     });
+// }
+
+//将数据存储到缓存
+Storage.prototype.setExpire=(key, value, expire) =>{
+    let obj={
+        data:value,
+        time:Date.now(),
+        expire:expire
+    };
+    localStorage.setItem(key,JSON.stringify(obj));
+}
+
+//从缓存中读取数据
+Storage.prototype.getExpire= key =>{
+    let val =localStorage.getItem(key);
+    if(!val){
+        return val;
+    }
+    val =JSON.parse(val);
+        if(Date.now()-val.time>val.expire){
+        localStorage.removeItem(key);
+        return null;
+    }
+    return val.data;
+}
+
+
+function initInfo(userVO){//对个人信息进行赋值
+    //姓名
+    var nameDD = document.getElementById("infoName");
+    nameDD.innerHTML = userVO.user.name;
+    //账号（学号）
+    var accountDD = document.getElementById("infoAccount");
+    accountDD.innerHTML = userVO.user.account;
+    //等级
+    var levelDD = document.getElementById("infoLevel");
+    levelDD.innerHTML = userVO.accountData.level;
+    //积分
+    var scoreDD = document.getElementById("infoScore");
+    scoreDD.innerHTML = userVO.accountData.score;
+    //提问数
+    var quesNumDD = document.getElementById("infoQuesNum");
+    quesNumDD.innerHTML = userVO.accountData.questionNum;
+    //回复数
+    var respNumDD = document.getElementById("infoRespNum");
+    respNumDD.innerHTML = userVO.accountData.responseNum;
+    //关注数
+    var focusNumDD = document.getElementById("infoFocusNum");
+    focusNumDD.innerHTML = userVO.accountData.focusNum;
+}
+
+//获取格式为yyyy-MM-dd HH-mm-ss格式的时间字符串
+function getTimeString(date){
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    if(month < 10){
+        month = "0" + month;
+    }
+
+    var strDate = date.getDate();
+    if(strDate < 10){
+        strDate = "0" + strDate;
+    }
+
+    var hour = date.getHours();
+    if(hour < 10){
+        hour = "0" + hour;
+    }
+
+    var min = date.getMinutes(); 
+    if(min < 10){
+        min = "0" + min;
+    }
+    
+    var sec = date.getSeconds(); 
+    if(sec < 10){
+        sec = "0" + sec;
+    }
+
+    return "" + year + "-" + month + "-" + strDate + " " + hour + ":" + min + ":" + sec;
+}
+
+//创建问题
+function postQuestion(){
+    var title =  $.trim($("#inputQue").val());
+    var content = $.trim($("#describeQue").val());
+    //创建时间
+    var time = new Date();
+    var timeString = getTimeString(time);
+    var question={
+        "title":title,
+        "content":content,
+        "question":{
+            "authorId":userVO.user.id,
+            "createTime":timeString
+        }
+    }
+    $.ajax({
+        url: "http://118.190.90.167:8888/question",
+        type: "post", 
+        data:JSON.stringify(question),
+        beforeSend: function (request) {
+            request.setRequestHeader("token", userVO.token);
+        },
+        contentType: 'application/json;charset=utf-8',
+        success: function (data) {
+            updateUser(data.data)
+        }
+    });
+    //将输入内容清空
+    $("#inputQue").innerHTML = "";
+    $("#describeQue").innerHTML = "";
+    location.replace(document.referrer);
+}
+
+
+//判断后台是否有临时板块（有则显示，无则隐藏）
+function getBlock(){
+    //获取临时板块的元素
+    var tempBlock = document.getElementById("tempBlock");
+    $.ajax({
+        url: "http://118.190.90.167:8888/block",
+        type: "get", 
+        contentType: 'application/json;charset=utf-8',
+        beforeSend: function (request) {
+            request.setRequestHeader("token", userVO.token);
+        },
+        success: function (data) {
+            if(data.code == 200){
+                block = data.data;
+                //有临时板块
+                //展示临时板块按钮
+                tempBlock.removeAttribute("style");
+                var blockLink = document.createElement("a");
+                blockLink.innerHTML = block.blockName;
+                var href = "search.html?keyWord=" +  block.keyWord;
+                blockLink.setAttribute("href",href);
+                tempBlock.appendChild(blockLink);
+                //console.info(block);
+            }else if(data.code == 201){
+                //没有临时板块
+                //隐藏临时板块按钮
+                tempBlock.setAttribute("style","display:none");
+            }
+        }
+    });
+}
+
+//获取临时板块实体
+function getBlockModel(){
+    $.ajax({
+        url: "http://118.190.90.167:8888/block",
+        type: "get", 
+        contentType: 'application/json;charset=utf-8',
+        beforeSend: function (request) {
+            request.setRequestHeader("token", userVO.token);
+        },
+        success: function (data) {
+            if(data.code == 200){
+                tempBlock = data.data;
+                //有临时板块
+            }else if(data.code == 201){
+                //没有临时板块
+                tempBlock = null;
+            }
+            //初始化界面
+            initBlock();
+        }
+    });
+}
+
+//更新问题列表
+function updateList(p){
+    page = p;
+    var list = page.questions;
+    list = Array.prototype.slice.call(list);
+    for(var i = 0; i < list.length; i++){
+        //创建存放单个问题的blockquote
+        var questionBlock = document.createElement("blockquote");
+        //创建存放问题标题的a标签
+        var title = document.createElement("a");
+        title.innerHTML = "标题：" + list[i].title;//问题标题
+        title.setAttribute("href", "questionIndex.html?id=" + list[i].question.id);//问题标题对应的详情页面
+        //创建存放问题描述的p标签
+        var content = document.createElement("small");
+        content.innerHTML = "问题描述：" + list[i].content;//问题描述
+        questionBlock.appendChild(title);
+        questionBlock.appendChild(content);
+        questionList.appendChild(questionBlock);
+        //更新分页按钮
+        updatePageButtons();
+    }
+}
+
+//将用户信息保存到缓存中(时间为30分钟)
+function updateUser(userVO){
+    localStorage.setExpire("userVO",userVO,1000*60*30);
+}
+
+
+
+//搜索框搜素函数
+function search(){
+    //搜索框输入内容
+    var searchContent = document.getElementById("searchContent");
+    var content = $.trim($("#searchContent").val());
+    window.location.href="search.html?keyWord=" + content;
+}
+
+//后台页面注销登录
+function adminExit(){
+    localStorage.setExpire("userVO",null,0);
+    location.href = "../login.html";
+}
+
+//前台页面注销登录
+function exit(){
+    localStorage.setExpire("userVO",userVO,1);
+    //alert("12")
+    //window.location.href = "my";
+
+}
+
+function changePage(index){
+    questionDiv.removeChild(questionList);
+    questionList = document.createElement("div");
+    questionDiv.insertBefore(questionList, pageButtons);
+    questionList.setAttribute("id","quetionList");
+    //pageButtons.removeAttribute("disabled");
+    page.pageIndex = index;
+    changeQuestionPage(page, path);
+}
+
+//上一页触发事件
+function prePage(flag, index){
+    if(flag == "1"){
+        changePage(index);
+    }else{
+        alert("抱歉，已经是第一页了");
+    }
+}
+
+//下一页触发事件
+function nextPage(flag, index){
+    if(flag == "1"){
+        changePage(index);
+    }else{
+        alert("抱歉，已经是最后一页了");
+    }
+}
+
+//更新分页按钮
+function updatePageButtons(){
+    questionDiv.removeChild(pageButtons);
+    pageButtons = document.createElement("ul");
+    pageButtons.className = "pagination pagination-lg";
+    pageButtons.id = "pageButtons";
+    questionDiv.insertBefore(pageButtons,divEnd);
+
+    //首页
+    var firstButton = document.createElement("li");
+    var firstLink = document.createElement("a");
+    firstLink.innerHTML = "首页";
+    firstButton.appendChild(firstLink)
+    pageButtons.appendChild(firstButton);
+    firstButton.setAttribute("onclick","changePage(" + 1 + ");");
+
+    //上一页
+    var preButton = document.createElement("li");
+    var preLink = document.createElement("a");
+    preLink.innerHTML = "上一页";
+    preButton.appendChild(preLink);
+    pageButtons.append(preButton);
+    preButton.setAttribute("id","previous");
+
+    if(page.hasPrevious == true){//有上一页
+        preButton.setAttribute("onclick", "prePage( 1, " + (page.pageIndex-1) + ");");
+    }else{//没有上一页
+        preButton.setAttribute("onclick", "prePage( 0, " + (page.pageIndex-1) + ");");
+    }
+
+    //分页页码列表
+    var buttonList = page.buttonList;
+    console.info(buttonList);
+
+    for(var i = 0; i < buttonList.length; i++){
+        var button_i = document.createElement("li")
+        var link_i = document.createElement("a");
+        link_i.innerHTML = buttonList[i];
+        button_i.appendChild(link_i);
+        pageButtons.appendChild(button_i);
+        button_i.setAttribute("onclick", "changePage(" + buttonList[i] + ");")
+        if(buttonList[i] == page.pageIndex){
+            link_i.setAttribute("style","color:red");
+        }
+    }
+
+    //下一页
+    var nextButton = document.createElement("li");
+    var nextLink = document.createElement("a");
+    nextLink.innerHTML = "下一页";
+    nextButton.appendChild(nextLink);
+    pageButtons.append(nextButton);
+    nextButton.setAttribute("id","next")
+    nextButton.setAttribute("onclick", "nextPage(" + (page.pageIndex+1) + ");");
+
+    if(page.hasNext == true){//有下一页
+        nextButton.setAttribute("onclick", "nextPage( 1, " + (page.pageIndex+1) + ");");
+    }else{//没有下一页
+        nextButton.setAttribute("onclick", "nextPage( 0, " + (page.pageIndex+1) + ");");
+    }
+
+    //末页
+    var lastButton = document.createElement("li");
+    var lastLink = document.createElement("a");
+    lastLink.innerHTML = "末页";
+    lastButton.appendChild(lastLink)
+    pageButtons.appendChild(lastButton);
+    lastButton.setAttribute("onclick", "changePage(" + page.pageNum + ");")
+}
+
+var userVO;
+//获取当前登录用户
+userVO = localStorage.getExpire("userVO");
+if(userVO != null){
+    updateUser(userVO);
+}
+
